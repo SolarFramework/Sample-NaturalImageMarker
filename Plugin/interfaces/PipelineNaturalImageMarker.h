bc0c185aefacbfe2c45b38f0f1e971021079590e
Jean-Bernard Rault <jean-bernard.rault@b-com.com>
2019-01-22 15:49:21 +0100

    first release of a natural image marker pipeline

---
+++ Plugin/interfaces/PipelineNaturalImageMarker.h
@@ -0,0 +1,192 @@
+/**
+ * @copyright Copyright (c) 2017 B-com http://www.b-com.com/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef PIPELINENATURALIMAGEMARKER_H
+#define PIPELINENATURALIMAGEMARKER_H
+
+#if _WIN32
+#ifdef PipelineNaturalImageMarker_API_DLLEXPORT
+#define SOLARPIPELINENATURALIMAGEMARKER_EXPORT_API __declspec(dllexport)
+#else //SOLARPIPELINEFIDUCIALMARKER_API_DLLEXPORT
+#define SOLARPIPELINENATURALIMAGEMARKER_EXPORT_API __declspec(dllimport)
+#endif //SOLARPIPELINEFIDUCIALMARKER_API_DLLEXPORT
+#else //_WIN32
+#define SOLARPIPELINENATURALIMAGEMARKER_EXPORT_API
+#endif //_WIN32
+
+#include "xpcf/core/traits.h"
+#include "xpcf/component/ConfigurableBase.h"
+#include "api/pipeline/IPipeline.h"
+
+// Add the headers to datastructures and component interfaces used by the pipeline
+#include "api/input/devices/ICamera.h"
+
+#include "api/input/files/IMarker2DNaturalImage.h"
+#include "api/features/IKeypointDetector.h"
+#include "api/features/IDescriptorsExtractor.h"
+#include "api/features/IMatchesFilter.h"
+#include "api/solver/pose/I2DTransformFinder.h"
+#include "api/solver/pose/IHomographyValidation.h"
+#include "api/features/IKeypointsReIndexer.h"
+#include "api/solver/pose/I3DTransformFinderFrom2D3D.h"
+#include "api/geom/IImage2WorldMapper.h"
+#include "api/geom/I2DTransform.h"
+
+#include "api/input/files/IMarker2DSquaredBinary.h"
+#include "api/image/IImageFilter.h"
+#include "api/image/IImageConvertor.h"
+#include "api/features/IContoursExtractor.h"
+#include "api/features/IContoursFilter.h"
+#include "api/image/IPerspectiveController.h"
+#include "api/features/IDescriptorsExtractorSBPattern.h"
+#include "api/features/IDescriptorMatcher.h"
+#include "api/features/ISBPatternReIndexer.h"
+
+#include "api/features/IKeypointDetector.h"
+#include "api/features/IDescriptorsExtractor.h"
+#include "api/features/IMatchesFilter.h"
+#include "api/solver/pose/I2DTransformFinder.h"
+#include "api/solver/pose/IHomographyValidation.h"
+#include "api/features/IKeypointsReIndexer.h"
+#include "api/solver/pose/I3DTransformFinderFrom2D3D.h"
+#include "api/geom/IImage2WorldMapper.h"
+#include "api/geom/I2DTransform.h"
+
+#ifdef USE_OPENGL
+    #include "api/sink/ISinkPoseTextureBuffer.h"
+#else
+    #include "api/sink/ISinkPoseImage.h"
+#endif
+
+#include "xpcf/threading/SharedBuffer.h"
+#include "xpcf/threading/DropBuffer.h"
+#include "xpcf/threading/BaseTask.h"
+
+namespace SolAR {
+using namespace datastructure;
+using namespace api;
+using namespace api::sink;
+using namespace api::pipeline;
+namespace PIPELINES {
+
+/**
+ * @class FiducialMarker
+ * @brief A pipeline to estimate the pose based on a squared fiducial marker.
+ */
+
+class SOLARPIPELINENATURALIMAGEMARKER_EXPORT_API PipelineNaturalImageMarker : public org::bcom::xpcf::ConfigurableBase,
+    public api::pipeline::IPipeline
+{
+public:
+    PipelineNaturalImageMarker();
+    ~PipelineNaturalImageMarker();
+
+    //// @brief Initialization of the pipeline
+    /// Initialize the pipeline by providing a reference to the component manager loaded by the PipelineManager.
+    /// @param[in] componentManager a shared reference to the component manager which has loaded the components and configuration in the pipleine manager
+    FrameworkReturnCode init(SRef<xpcf::IComponentManager> xpcfComponentManager) override;
+
+    /// @brief Provide the camera parameters
+    /// @return the camera parameters (its resolution and its focal)
+    CameraParameters getCameraParameters() override;
+
+    /// @brief Starts the pipeline and provides a texture buffer which will be updated when required.
+    /// @param[in] textureHandle a pointer to the texture buffer which will be updated at each call of the update method.
+#ifdef USE_OPENGL
+    FrameworkReturnCode start(void* textureHandle) override;
+#else
+    FrameworkReturnCode start(void* imageDataBuffer) override;
+#endif
+
+    /// @brief Stop the pipeline.
+    FrameworkReturnCode stop() override;
+
+    /// @brief update the pipeline
+    /// Get the new pose and update the texture buffer with the image that has to be displayed
+    SinkReturnCode update(Transform3Df& pose) override;
+
+    void updateFrameDataOGL(int enventID) override;
+
+    void unloadComponent () override final;
+
+private:
+    // Decalaration of data structures shared between initialization and process thread
+    SRef<DescriptorBuffer> m_markerPatternDescriptor;
+
+    // Declaration of the components used by the pipeline
+    SRef<input::devices::ICamera> m_camera;
+    SRef<input::files::IMarker2DSquaredBinary> m_binaryMarker;
+
+    SRef<input::files::IMarker2DNaturalImage> m_naturalImagemarker;
+    SRef<features::IKeypointDetector> m_kpDetector;
+    SRef<features::IDescriptorMatcher> m_matcher;
+    SRef<features::IMatchesFilter> m_basicMatchesFilter;
+    SRef<features::IMatchesFilter> m_geomMatchesFilter;
+    SRef<solver::pose::I2DTransformFinder> m_homographyEstimation ;
+    SRef<solver::pose::IHomographyValidation> m_homographyValidation ;
+    SRef<features::IKeypointsReIndexer> m_keypointsReindexer;
+    SRef<solver::pose::I3DTransformFinderFrom2D3D> m_poseEstimation;
+    SRef<features::IDescriptorsExtractor> m_descriptorExtractor;
+    SRef<geom::IImage2WorldMapper> m_img_mapper;
+    SRef<geom::I2DTransform> m_transform2D;
+
+    SRef<image::IImageFilter> m_imageFilterBinary;
+    SRef<image::IImageConvertor> m_imageConvertor;
+    SRef<features::IContoursExtractor> m_contoursExtractor ;
+    SRef<features::IContoursFilter> m_contoursFilter;
+    SRef<image::IPerspectiveController> m_perspectiveController;
+    SRef<features::IDescriptorsExtractorSBPattern> m_patternDescriptorExtractor;
+    SRef<features::IDescriptorMatcher> m_patternMatcher;
+    SRef<features::ISBPatternReIndexer> m_patternReIndexer;
+    SRef<geom::IImage2WorldMapper> m_img2worldMapper;
+    SRef<solver::pose::I3DTransformFinderFrom2D3D> m_PnP;
+#ifdef USE_OPENGL
+    SRef<sink::ISinkPoseTextureBuffer> m_sink;
+#else
+    SRef<sink::ISinkPoseImage> m_sink;
+#endif
+    // State flag of the pipeline
+    bool m_stopFlag, m_initOK, m_startedOK;
+
+    // Threads
+    bool processCamImage();
+    xpcf::DelegateTask* m_taskProcess;
+
+private :
+    SRef<DescriptorBuffer>  m_refDescriptors,  m_camDescriptors;
+    std::vector<DescriptorMatch>  m_matches;
+
+    Transform2Df  m_Hm;
+    std::vector< SRef<Keypoint> >  m_refKeypoints,  m_camKeypoints;  // where to store detected keypoints in ref image and camera image
+
+    std::vector<SRef <Point2Df>> m_refImgCorners;
+
+    SRef<Image> m_refImage;
+    SRef<Image> m_camImage;
+
+    Transform3Df m_pose;
+
+};
+
+}
+}
+
+XPCF_DEFINE_COMPONENT_TRAITS(SolAR::PIPELINES::PipelineNaturalImageMarker,
+                             "6f6c7ae8-764c-49b3-a2fc-e9a4f539b9b1",
+                             "PipelineNaturalImageMarker",
+                             "A pipeline to estimate the pose based on a natural Image marker");
+
+#endif // PIPELINENATURALIMAGEMARKER_H
---
+++ Plugin/tests/TestNaturalImageMarkerPipeline/main.cpp
@@ -0,0 +1,208 @@
+/**
+ * @copyright Copyright (c) 2017 B-com http://www.b-com.com/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <boost/log/core.hpp>
+#include "core/Log.h"
+#include "xpcf/xpcf.h"
+
+
+// ADD COMPONENTS HEADERS HERE, e.g #include "SolarComponent.h"
+
+#include "PipelineManager.h"
+#include "freeglut.h"
+
+using namespace SolAR;
+using namespace SolAR::PIPELINE;
+
+namespace xpcf  = org::bcom::xpcf;
+
+#define MARKER_CONFIGFILE "NaturalImageMarker.yml"
+#define CAMERA_CONFIGFILE "camera_calibration.yml"
+
+#include "SolARModuleOpencv_traits.h"
+#include "SolARImageViewerOpencv.h"
+#include "SolAR3DOverlayBoxOpencv.h"
+
+using namespace SolAR;
+using namespace SolAR::MODULES::OPENCV;
+using namespace SolAR::api;
+
+int ParseMarkerConfig(const char* fileName,float& width,float& height)
+{
+    cv::FileStorage fs(fileName, cv::FileStorage::READ);
+
+    if (fs.isOpened())
+    {
+        fs["MarkerWidth"] >> width;
+        fs["MarkerHeight"] >> height;
+    }
+    else
+    {
+        LOG_ERROR("SolARCameraOpencv::loadCameraParameters: Cannot open marker calibration file ")
+        return 1;
+    }
+    return 0;
+}
+
+
+int ParseCameraConfig(const char* fileName,CamCalibration& intrinsic_param,CamDistortion&  distorsion_param)
+{
+    cv::FileStorage fs(fileName, cv::FileStorage::READ);
+    cv::Mat intrinsic_parameters;
+    cv::Mat distortion_parameters;
+
+    if (fs.isOpened())
+    {
+        int width, height;
+        fs["image_width"] >> width;
+        fs["image_height"] >> height;
+        fs["camera_matrix"] >> intrinsic_parameters;
+        fs["distortion_coefficients"] >> distortion_parameters;
+
+//        m_resolution.width = width;
+//        m_resolution.height = height;
+//        m_is_resolution_set = true;
+
+        if (intrinsic_parameters.empty())
+        {
+            LOG_ERROR ("SolARCameraOpencv::loadCameraParameters: Use the landmark camera_matrix to define the intrinsic matrix in the .yml camera calibration file")
+            return xpcf::_FAIL;
+        }
+
+        if (intrinsic_parameters.rows == intrinsic_param.rows() && intrinsic_parameters.cols == intrinsic_param.cols())
+            for (int i = 0; i < intrinsic_parameters.rows; i++)
+                for (int j = 0; j < intrinsic_parameters.cols; j++)
+                    intrinsic_param(i,j) = (float)intrinsic_parameters.at<double>(i,j);
+        else
+        {
+            LOG_ERROR("SolARCameraOpencv::loadCameraParameters: Camera Calibration should be a 3x3 Matrix")
+            return 1;
+        }
+
+        if (distortion_parameters.empty())
+        {
+            LOG_ERROR("SolARCameraOpencv::loadCameraParameters: Use the landmark distortion_coefficients to define the distortion vector in the .yml camera calibration file")
+            return xpcf::_FAIL;
+        }
+
+        if (distortion_parameters.rows == distorsion_param.rows() && distortion_parameters.cols == distorsion_param.cols())
+            for (int i = 0; i < distortion_parameters.rows; i++)
+                for (int j = 0; j < distortion_parameters.cols; j++)
+                    distorsion_param(i,j) = distortion_parameters.at<double>(i,j);
+        else
+        {
+            LOG_ERROR("SolARCameraOpencv::loadCameraParameters: Camera distortion matrix should be a 5x1 Matrix")
+            return 1;
+        }
+        return 0;
+    }
+    else
+    {
+        LOG_ERROR("SolARCameraOpencv::loadCameraParameters: Cannot open camera calibration file ")
+        return 1;
+    }
+}
+int main(){
+
+     auto imageViewerResult =xpcf::ComponentFactory::createInstance<SolARImageViewerOpencv>()->bindTo<display::IImageViewer>();
+     auto overlay3DComponent =xpcf::ComponentFactory::createInstance<SolAR3DOverlayBoxOpencv>()->bindTo<display::I3DOverlay>();
+
+     overlay3DComponent->bindTo<xpcf::IConfigurable>()->getProperty("position")->setFloatingValue(0,0);
+     overlay3DComponent->bindTo<xpcf::IConfigurable>()->getProperty("position")->setFloatingValue(0,1);
+     overlay3DComponent->bindTo<xpcf::IConfigurable>()->getProperty("position")->setFloatingValue(0,2);
+
+     overlay3DComponent->bindTo<xpcf::IConfigurable>()->getProperty("orientation")->setFloatingValue(0,0);
+     overlay3DComponent->bindTo<xpcf::IConfigurable>()->getProperty("orientation")->setFloatingValue(0,1);
+     overlay3DComponent->bindTo<xpcf::IConfigurable>()->getProperty("orientation")->setFloatingValue(0,2);
+
+     float marker_width,marker_height;
+     if(ParseMarkerConfig(MARKER_CONFIGFILE,marker_width,marker_height)!=0){
+         LOG_INFO("pb parsing config file {} \n",MARKER_CONFIGFILE);
+         return 1;
+     }
+
+     overlay3DComponent->bindTo<xpcf::IConfigurable>()->getProperty("size")->setFloatingValue(marker_width,0);
+     overlay3DComponent->bindTo<xpcf::IConfigurable>()->getProperty("size")->setFloatingValue(marker_height,1);
+     overlay3DComponent->bindTo<xpcf::IConfigurable>()->getProperty("size")->setFloatingValue(0.1,2);
+
+     overlay3DComponent->bindTo<xpcf::IConfigurable>()->onConfigured();
+
+     CamCalibration intrinsic_param ;
+     CamDistortion  distorsion_param ;
+     if(ParseCameraConfig(CAMERA_CONFIGFILE,intrinsic_param,distorsion_param)!=0){
+         LOG_INFO("pb parsinf camera config file \n")
+         return 1;
+     }
+     overlay3DComponent->setCameraParameters(intrinsic_param, distorsion_param);
+
+
+
+#if NDEBUG
+    boost::log::core::get()->set_logging_enabled(false);
+#endif
+
+    LOG_ADD_LOG_TO_CONSOLE();
+
+    PipelineManager pipeline;
+    if (pipeline.init("PipelineNaturalImageMarker.xml", "6f6c7ae8-764c-49b3-a2fc-e9a4f539b9b1"))
+    {
+
+        PipelineManager::CamParams calib = pipeline.getCameraParameters();
+
+        unsigned char* r_imageData=new unsigned char[calib.width*calib.height*3];
+        SRef<Image> camImage;
+        Transform3Df s_pose;
+
+        if (pipeline.start(r_imageData))
+        {
+            while (true)
+            {
+                PipelineManager::Pose pose;
+
+                camImage=xpcf::utils::make_shared<Image>(r_imageData,calib.width,calib.height,SolAR::Image::LAYOUT_BGR,SolAR::Image::INTERLEAVED,SolAR::Image::TYPE_8U);
+
+                PIPELINEMANAGER_RETURNCODE returnCode = pipeline.udpate(pose);
+                if(returnCode==PIPELINEMANAGER_RETURNCODE::_ERROR)
+                    break;
+
+                if ((returnCode & PIPELINEMANAGER_RETURNCODE::_NEW_POSE))
+                {
+                    LOG_INFO("Camera Pose translation ({}, {}, {})", pose.translation(0), pose.translation(1), pose.translation(2));
+                    for(int i=0;i<3;i++)
+                         for(int j=0;j<3;j++)
+                             s_pose(i,j)=pose.rotation(i,j);
+                    for(int i=0;i<3;i++)
+                             s_pose(i,3)=pose.translation(i);
+                    for(int j=0;j<3;j++)
+                        s_pose(3,j)=0;
+                    s_pose(3,3)=1;
+                    LOG_INFO("pose.matrix():\n {} \n",s_pose.matrix())
+                    overlay3DComponent->draw(s_pose, camImage);
+                }
+
+                if (imageViewerResult->display(camImage) == SolAR::FrameworkReturnCode::_STOP){
+                    pipeline.stop();
+                    break;
+                }
+             }
+        }
+    }
+}
+
+
+
+
+
