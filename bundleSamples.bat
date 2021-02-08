8ce099bb806158143dd5baa7b5e725eb7b387aa0
BCOM\jroyan <jerome.royan@b-com.com>
2021-02-08 10:02:46 +0100

    Add a script to bundle sample on Windows

---
+++ bundleSamples.bat
@@ -0,0 +1,41 @@
+@echo off
+SETLOCAL EnableDelayedExpansion
+
+SET version=0.9.0
+
+SET filename=SolAR_Fiducial_%version%
+SET arg1=%1
+
+IF NOT "!arg1!"=="" (SET filename=%arg1%)
+echo filename is %filename%
+
+
+
+echo "**** Install dependencies locally"
+:: remaken install packagedependencies.txt
+:: remaken install packagedependencies.txt -c debug
+
+echo "**** Bundle dependencies in bin folder"
+ FOR /D /R %%d IN (SolARSample*) DO (
+    For %%f IN (%%~fd\*_conf.xml) DO (
+      echo "** Bundle sample configuration file %%f"
+      remaken bundleXpcf "%%f" -d ./bin/Release -s modules
+      remaken bundleXpcf "%%f" -d ./bin/Debug -s modules -c debug
+   )
+)
+
+FOR /D /R %%d IN (SolARPipeline*) DO (
+   For %%f IN (%%~fd\*_conf.xml) DO (
+      echo "** Bundle sample configuration file %%f"
+      remaken bundleXpcf "%%f" -d ./bin/Release -s modules
+      remaken bundleXpcf "%%f" -d ./bin/Debug -s modules -c debug
+   )
+)
+
+
+echo "**** Zip bundles"
+"7z.exe" a -tzip bin\%filename%_debug.zip README.md
+"7z.exe" a -tzip bin\%filename%_release.zip README.md
+"7z.exe" a -tzip bin\%filename%_debug.zip bin\Debug
+"7z.exe" a -tzip bin\%filename%_release.zip bin\Release
+
