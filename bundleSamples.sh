071b1ea35c98ac0e58922d7c3a16da331cfb248e
BCOM\jroyan <jerome.royan@b-com.com>
2021-02-18 18:07:01 +0100

    Remove the copy of installData.sh as no external data are required for these samples.

--- bundleSamples.sh
+++ bundleSamples.sh
@@ -33,5 +33,5 @@ cp ./runFromBundle.sh ./run.sh
 mv ./run.sh ./bin/Debug
 
 
-zip --symlinks -r "./bin/${filename}_release.zip" ./bin/Release ./README.md ./installData.sh ./LICENSE
-zip --symlinks -r "./bin/${filename}_debug.zip" ./bin/Debug ./README.md ./installData.sh ./LICENSE 
+zip --symlinks -r "./bin/${filename}_release.zip" ./bin/Release ./README.md ./LICENSE
+zip --symlinks -r "./bin/${filename}_debug.zip" ./bin/Debug ./README.md ./LICENSE 
