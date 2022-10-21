0e81234b722de61a41ced071b316aad5f159fe8c
Jean-Marie HENAFF <Jean-Marie.HENAFF@b-com.com>
2022-10-21 11:14:10 +0200

    Use a dedicated directory for deployment
    Avoid to mess with root dir of projects and overwriting of files such as packagedependencies.txt.

--- bundleSamples.bat
+++ bundleSamples.bat
@@ -19,25 +19,25 @@ echo "**** Bundle dependencies in bin folder"
  FOR /D /R %%d IN (SolARSample*) DO (
     For %%f IN (%%~fd\*_conf.xml) DO (
       echo "** Bundle sample configuration file %%f"
-      remaken bundleXpcf "%%f" -d ./bin/x86_64/shared/release -s modules
-      remaken bundleXpcf "%%f" -d ./bin/x86_64/shared/debug -s modules -c debug
+      remaken bundleXpcf "%%f" -d ./deploy/bin/x86_64/shared/release -s modules
+      remaken bundleXpcf "%%f" -d ./deploy/bin/x86_64/shared/debug -s modules -c debug
    )
 )
 
 FOR /D /R %%d IN (SolARPipeline*) DO (
    For %%f IN (%%~fd\*_conf.xml) DO (
       echo "** Bundle sample configuration file %%f"
-      remaken bundleXpcf "%%f" -d ./bin/x86_64/shared/release -s modules
-      remaken bundleXpcf "%%f" -d ./bin/x86_64/shared/debug -s modules -c debug
+      remaken bundleXpcf "%%f" -d ./deploy/bin/x86_64/shared/release -s modules
+      remaken bundleXpcf "%%f" -d ./deploy/bin/x86_64/shared/debug -s modules -c debug
    )
 )
 
 
 echo "**** Zip bundles"
-"7z.exe" a -tzip bin\%filename%_debug.zip README.md
-"7z.exe" a -tzip bin\%filename%_release.zip README.md
-"7z.exe" a -tzip bin\%filename%_debug.zip LICENSE
-"7z.exe" a -tzip bin\%filename%_release.zip LICENSE
-"7z.exe" a -tzip bin\%filename%_debug.zip bin\x86_64\shared\debug
-"7z.exe" a -tzip bin\%filename%_release.zip bin\x86_64\shared\release
+"7z.exe" a -tzip deploy\%filename%_debug.zip README.md
+"7z.exe" a -tzip deploy\%filename%_release.zip README.md
+"7z.exe" a -tzip deploy\%filename%_debug.zip LICENSE
+"7z.exe" a -tzip deploy\%filename%_release.zip LICENSE
+"7z.exe" a -tzip deploy\%filename%_debug.zip deploy\bin\x86_64\shared\debug
+"7z.exe" a -tzip deploy\%filename%_release.zip deploy\bin\x86_64\shared\release
 
