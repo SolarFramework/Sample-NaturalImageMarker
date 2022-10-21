0b6176c7ee0cf310683a84f9a7c9310e60d2f716
Jean-Marie HENAFF <Jean-Marie.HENAFF@b-com.com>
2022-10-21 11:14:10 +0200

    Use a dedicated directory for deployment
    Avoid to mess with root dir of projects and overwriting of files such as packagedependencies.txt.
    
    (cherry picked from commit 0e81234b722de61a41ced071b316aad5f159fe8c)

--- bundleSamples.sh
+++ bundleSamples.sh
@@ -16,15 +16,15 @@ echo "**** Bundle dependencies in bin folder"
 for file in $(find ./SolARSample* ./SolARPipeline*/tests/SolARPipelineTest* -path "*_conf.xml")
 do
    echo "install dependencies for config file: $file"
-   remaken bundleXpcf $file -d ./bin/x86_64/shared/release -s modules
-   remaken bundleXpcf $file -d ./bin/x86_64/shared/debug -s modules -c debug
+   remaken bundleXpcf $file -d ./deploy/bin/x86_64/shared/release -s modules
+   remaken bundleXpcf $file -d ./deploy/bin/x86_64/shared/debug -s modules -c debug
 done
 
 cp ./runFromBundle.sh ./run.sh
-mv ./run.sh ./bin/x86_64/shared/release/
+mv ./run.sh ./deploy/bin/x86_64/shared/release/
 cp ./runFromBundle.sh ./run.sh
-mv ./run.sh ./bin/x86_64/shared/debug
+mv ./run.sh ./deploy/bin/x86_64/shared/debug
 
 
-zip --symlinks -r "./bin/${filename}_release.zip" ./bin/x86_64/shared/release ./README.md ./LICENSE
-zip --symlinks -r "./bin/${filename}_debug.zip" ./bin/x86_64/shared/debug ./README.md ./LICENSE 
+zip --symlinks -r "./deploy/${filename}_release.zip" ./deploy/bin/x86_64/shared/release ./README.md ./LICENSE
+zip --symlinks -r "./deploy/${filename}_debug.zip" ./deploy/bin/x86_64/shared/debug ./README.md ./LICENSE 
