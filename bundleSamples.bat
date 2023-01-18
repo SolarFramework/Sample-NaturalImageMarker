acdf8344907e670641376d962dda06db01cd7c69
Jean-Marie HENAFF <Jean-Marie.HENAFF@b-com.com>
2023-01-18 11:39:45 +0100

    Add error handling in bundleSamples.bat

--- bundleSamples.bat
+++ bundleSamples.bat
@@ -12,32 +12,32 @@ echo filename is %filename%
 
 
 echo "**** Install dependencies locally"
-remaken install packagedependencies.txt
-remaken install packagedependencies.txt -c debug
+remaken install packagedependencies.txt || (echo "Error: could not remaken install release deps" && EXIT /B 1)
+remaken install packagedependencies.txt -c debug || (echo "Error: could not remaken install debug deps" && EXIT /B 1)
 
 echo "**** Bundle dependencies in bin folder"
  FOR /D /R %%d IN (SolARSample*) DO (
     For %%f IN (%%~fd\*_conf.xml) DO (
       echo "** Bundle sample configuration file %%f"
-      remaken bundleXpcf --recurse -d ./deploy/bin/x86_64/shared/release -s modules "%%f"
-      remaken bundleXpcf --recurse -d ./deploy/bin/x86_64/shared/debug -s modules -c debug "%%f"
+      remaken bundleXpcf --recurse -d ./deploy/bin/x86_64/shared/release -s modules "%%f" || (echo "Error: could not remaken bundleXpcf release deps of SolAR sample (%%f)" && EXIT /B 1)
+      remaken bundleXpcf --recurse -d ./deploy/bin/x86_64/shared/debug -s modules -c debug "%%f" || (echo "Error: could not remaken bundleXpcf debug deps of SolAR sample (%%f)" && EXIT /B 1)
    )
 )
 
 FOR /D /R %%d IN (SolARPipeline*) DO (
    For %%f IN (%%~fd\*_conf.xml) DO (
       echo "** Bundle sample configuration file %%f"
-      remaken bundleXpcf --recurse -d ./deploy/bin/x86_64/shared/release -s modules "%%f"
-      remaken bundleXpcf --recurse -d ./deploy/bin/x86_64/shared/debug -s modules -c debug "%%f"
+      remaken bundleXpcf --recurse -d ./deploy/bin/x86_64/shared/release -s modules "%%f" || (echo "Error: could not remaken bundleXpcf release deps of SolAR pipeline (%%f)" && EXIT /B 1)
+      remaken bundleXpcf --recurse -d ./deploy/bin/x86_64/shared/debug -s modules -c debug "%%f" || (echo "Error: could not remaken bundleXpcf debug deps of SolAR pipeline (%%f)" && EXIT /B 1)
    )
 )
 
 
 echo "**** Zip bundles"
-"7z.exe" a -tzip deploy\%filename%_debug.zip README.md
-"7z.exe" a -tzip deploy\%filename%_release.zip README.md
-"7z.exe" a -tzip deploy\%filename%_debug.zip LICENSE
-"7z.exe" a -tzip deploy\%filename%_release.zip LICENSE
-"7z.exe" a -tzip deploy\%filename%_debug.zip deploy\bin\x86_64\shared\debug
-"7z.exe" a -tzip deploy\%filename%_release.zip deploy\bin\x86_64\shared\release
+("7z.exe" a -tzip deploy\%filename%_debug.zip README.md && ^
+"7z.exe" a -tzip deploy\%filename%_release.zip README.md && ^
+"7z.exe" a -tzip deploy\%filename%_debug.zip LICENSE && ^
+"7z.exe" a -tzip deploy\%filename%_release.zip LICENSE && ^
+"7z.exe" a -tzip deploy\%filename%_debug.zip deploy\bin\x86_64\shared\debug && ^
+"7z.exe" a -tzip deploy\%filename%_release.zip deploy\bin\x86_64\shared\release) || (echo "Error: could not create bundle archive" && EXIT /B 1)
 
