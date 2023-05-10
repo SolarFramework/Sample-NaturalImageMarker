59cb68f071af1d3b31802d09789b8ef4f757d592
Jean-Marie HENAFF <Jean-Marie.HENAFF@b-com.com>
2023-05-10 17:58:18 +0200

    Fix version and name in _registry
    Also change name field in module declaration i cpp source, and update version syntax in bundleSample.sh

--- SolARPipeline_NaturalImageMarker/src/PipelineNaturalImageMarker.cpp
+++ SolARPipeline_NaturalImageMarker/src/PipelineNaturalImageMarker.cpp
@@ -21,7 +21,7 @@
 namespace xpcf=org::bcom::xpcf;
 
 // Declaration of the module embedding the fiducial marker pipeline
-XPCF_DECLARE_MODULE("5bbc1452-8d7f-4512-83f1-7bf48453809f", "NaturalImageMarkerModule", "The module embedding a pipeline to estimate the pose based on a natural image marker")
+XPCF_DECLARE_MODULE("5bbc1452-8d7f-4512-83f1-7bf48453809f", "SolARPipelineNaturalImageMarker", "The module embedding a pipeline to estimate the pose based on a natural image marker")
 
 extern "C" XPCF_MODULEHOOKS_API xpcf::XPCFErrorCode XPCF_getComponent(const boost::uuids::uuid& componentUUID,SRef<xpcf::IComponentIntrospect>& interfaceRef)
 {
--- bundleSamples.sh
+++ bundleSamples.sh
@@ -1,6 +1,6 @@
 set -e
 
-Version="1.0.0"
+Version=1.0.0
 
 if [ -z "$1" ]
 then
